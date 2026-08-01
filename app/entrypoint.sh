a8b4283ade5d3c15d2cd1af1be225bbb06419dab
Yajas Sharma <yajassharma@Yajass-MacBook-Air.local>
2026-08-01 11:51:06 +0530

    chore: add Railway deployment configs (railway.toml, entrypoint scripts for PORT adaptation)

---
+++ app/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+PORT=${PORT:-80}
+sed -i "s/listen 80;/listen $PORT;/g" /etc/nginx/conf.d/default.conf
+sed -i "s/listen       80/listen       $PORT/g" /etc/nginx/conf.d/default.conf
+sed -i "s/listen  \[::\]:80/listen  [::]:$PORT/g" /etc/nginx/conf.d/default.conf
+exec nginx -g 'daemon off;'
